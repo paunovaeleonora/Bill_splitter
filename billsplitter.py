acfaef11e63f75725a1e8ddeabc0269185043ff9
paunovaeleonora <nora.paunova@gmail.com>
2021-10-21 22:06:18 +0100

    BiLL Splitter

--- billsplitter.py
+++ billsplitter.py
@@ -1,46 +1,32 @@
 import random
 
 
-def is_it_integer(n):
-    if not n.is_integer():
-        return round(n, 2)
-    return int(n)
-
-
-def split_the_bill(bill, n):
-    split = bill / n
-    return is_it_integer(split)
-
-
-people_going_to_party = []
-people_and_bill = {}
-number_of_people_joining_the_party = int(input('Enter the number of friends joining(including you):\n'))
-
-if number_of_people_joining_the_party <= 0:
+friends = []
+final_data = {}
+participants = int(input('Enter the number of friends joining(including you):\n'))
+if participants <= 0:
     print('\nNo one is joining for the party')
 
 else:
     print('\nEnter the name of every friend (including you), each on a new line:')
-    people_going_to_party=[input() for _ in range(number_of_people_joining_the_party)]
-    total_bill = int(input('\nEnter the total bill value:\n'))
-
+    for _ in range(participants):
+        name = input()
+        friends.append(name)
+    print('\nEnter the total bill value:')
+    final_bill = int(input())
+    # split_bill = final_bill / participants
+    # if not split_bill.is_integer():
+    #     split_bill = round(split_bill, 2)
+    # else:
+    #     split_bill = int(split_bill)
     print('\nDo you want to use the "Who is lucky?" feature? Write Yes/No:')
     answer = input()
     if answer == 'Yes':
-        number_of_people_joining_the_party -= 1
-        split_bill = split_the_bill(total_bill, number_of_people_joining_the_party)
-        lucky_one = random.choice(people_going_to_party)
-        for name in people_going_to_party:
-            if name == lucky_one:
-                people_and_bill[name] = 0
-            else:
-                people_and_bill[name] = split_bill
-
-        print(f'\n{lucky_one} is the lucky one!\n')
-        print(people_and_bill)
-
+        print(f'\n{random.choice(friends)} is the lucky one!')
     else:
-        split_bill = split_the_bill(total_bill, number_of_people_joining_the_party)
         print('\nNo one is going to be lucky')
-        people_and_bill = {friend: split_bill for friend in people_going_to_party}
-        print(f'\n{people_and_bill}')
+
+    # for friend in friends:
+    #     final_data[friend] = split_bill
+    # print()
+    # print(final_data)
